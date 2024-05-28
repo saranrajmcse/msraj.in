3486c999b064dd7e81351be9a39bfb05459becf6
saranrajmcse <saranrajmcse@gmail.com>
2024-05-28 09:37:22 +0530

    overlay

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 ﻿if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("service_worker.js").then(registration => {
+    navigator.serviceWorker.register("service-worker.js").then(registration => {
         console.log("SW Registered!");
     }).catch(error => {
         console.log("SW Registration Failed");
